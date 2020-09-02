2afc532b70a8f85a0909b983b744dd6c32822979
root <dtvangogh@github.com>
2020-09-02 02:40:17 +0000

    add

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,17 +1,37 @@
 #!/usr/bin/python3
-"""c/<routeVariable> route
 """
-from flask import Flask
+Flask web application
+"""
 
+from flask import Flask
 
 app = Flask(__name__)
 
 
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+        Displays hello message
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+        Displays the name of application
+    """
+    return "HBNB"
+
+
 @app.route('/c/<text>', strict_slashes=False)
-def get_text(text):
-    """get var from route"""
-    return 'C ' + text.replace('_', ' ')
+def croute(text):
+    """
+        Displays C followed by text variable
+
+    """
+    return "C %s" % text.replace('_', ' ')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0')
