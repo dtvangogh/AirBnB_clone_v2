b920f51ad7a4625a079ee52b8f912ebbcc2a5c85
root <dtvangogh@github.com>
2020-09-02 02:29:26 +0000

    add

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""HBNB route
+"""HBNB
 """
 from flask import Flask
 
@@ -9,9 +9,9 @@ app = Flask(__name__)
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """hbnb route"""
+    """Displays HBNB when curled"""
     return "HBNB"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0')
